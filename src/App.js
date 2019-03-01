15b5a3e373df2fe76ff2b935cafe85d0c2a4120a
M Haidar Hanif <git@mhaidarhanif.com>
2019-03-01 16:34:02 +0700

    Setup Redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import Home from './pages/Home'
 
+const initialState = {
+  todos: []
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SAMPLE': {
+      return {
+        todos: []
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const reduxStore = createStore(
+  reducer,
+  // Add Redux DevTools Extension
+  // https://github.com/zalmoxisus/redux-devtools-extension#11-basic-store
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 const App = () => {
-  return <Home />
+  return (
+    <Provider store={reduxStore}>
+      <Home />
+    </Provider>
+  )
 }
 
 export default App
